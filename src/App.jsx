0ef5191b9f437db57cb801eb2cc23870cf2ffe76
Catherine-Russell <catherinerussell123@hotmail.co.uk>
2024-02-17 20:22:56 +0000

    adds tests and code for day/month input

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,47 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [name, setName] = useState("");
+  const [day, setDay] = useState(0)
+  const [month, setMonth] = useState('')
+
 
 
   return (
     <>
       
       <h1>Hello There!</h1>
+
+      <form>
       <div className="card">
-        <h2 id="nameRequest">
-          What is your name?
+        <h2 id="name-request">
+          What&apos;s your name?
         </h2>
-        <input id="name" placeholder="name" value={name} onChange={ event => setName(event.target.value) }/>
+        <input id="name" aria-label="name-input" type="text" placeholder="name" value={name} onChange={ event => setName(event.target.value) }/>
         {/* onChange={e => setValue(e.target.value)}  */}
       </div>
 
+      <div className="card">
+        <h2 id="birthday-request">
+          When&apos;s your birthday?
+        </h2>
+        <div id="date input">
+          <h4>Day:</h4>
+          <input id="day" aria-label='day-input' type='number' value={ day } onChange={e => setDay(e.target.value)} />
+          <h4>Month:</h4>
+          <input id='month' aria-label='month-input' type='month' value={ month } onChange={e => setMonth(e.target.value)}/>
+        </div>
+      </div>
+      
+      <input type="submit" value="Submit" />
+
+      </form>
       
 
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </div> */}
     </>
   )
 }
--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -4,15 +4,10 @@ import { render, fireEvent, screen } from "@testing-library/react";
 
 describe("App component", () => {
   it("display Greeting", () => {
-    // Given
     const testComponent = (
       <App />
     );
-
-    // When
     render(testComponent);
-
-    // Then
     const appTitle = screen.getByText("Hello There!");
     expect(appTitle).toBeDefined();
   });
@@ -22,7 +17,7 @@ describe("App component", () => {
       <App />
     );
     render(testComponent);
-    expect(screen.getByText("What is your name?")).toBeDefined();
+    expect(screen.getByText("What's your name?")).toBeDefined();
   });
 
   it("has an input box for your name", () => {
@@ -30,7 +25,9 @@ describe("App component", () => {
       <App />
     );
     render(testComponent);
-    expect(screen.getByRole("textbox", { placeholder: "name" })).toBeDefined();
+    expect(screen.getByPlaceholderText("name")).toBeDefined();
+    // expect(screen.getByLabelText("name-input")).toBeDefined();
+
   });
 
   it("allows the user to type their name", () => {
@@ -38,7 +35,7 @@ describe("App component", () => {
       <App />
     );
     render(testComponent);
-    const inputField = screen.getByRole("textbox", { placeholder: "name"});
+    const inputField = screen.getByPlaceholderText("name");
 
     fireEvent.change(inputField, {target: {value: 'Test'}})
 
@@ -46,19 +43,55 @@ describe("App component", () => {
     expect(inputtedName).toBe("Test"); 
   });
 
-  it("", () => {
+  it("has a request for your birthday", () => {
+    render(<App />);
+    expect(screen.getByText("When's your birthday?")).toBeDefined();
+  });
+
+  it("prompts you to input the day", () => {
+    render(<App />);
+    expect(screen.getByText("Day:")).toBeDefined();
+  });
+
+  it("has an input box for the day of your birthday", () => {
     const testComponent = (
       <App />
     );
     render(testComponent);
-    const inputField = screen.getByRole("textbox", { placeholder: "name"});
-
-    fireEvent.change(inputField, {target: {value: 'Test'}})
-
-    const inputtedName = inputField.value
-    expect(inputtedName).toBe("Test"); 
+    expect(screen.getByLabelText("day-input")).toBeDefined();
   });
 
+  it("allows the user to update their birthday day", () => {
+    render(<App />);
+    const dayInputBox = screen.getByLabelText("day-input");
 
+    fireEvent.change(dayInputBox, {target: {value: '10'}})
+
+    expect(dayInputBox.value).toEqual('10')
+  });
+
+  it("prompts the user to input the month", () => {
+    render(<App />);
+    expect(screen.getByText("Month:")).toBeDefined()
+  });
+
+  it("has an input box for the month", () => {
+    render(<App />);
+    expect(screen.getByLabelText("month-input")).toBeDefined()
+  });
+
+  it("allows user to update bday month", () => {
+    render(<App />);
+    const monthInputField = screen.getByLabelText("month-input")
+    
+    fireEvent.change(monthInputField, {target: {value: "2020-01"}})
+
+    expect(monthInputField.value).toBe("2020-01")
+  });
+
+  it("has a button for user to submit info when completed", () => {
+    render(<App />);
+    expect(screen.getByRole("button")).toBeDefined()
+  })
 }
 )
